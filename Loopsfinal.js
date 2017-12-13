11d2d915d79ebc2b3a9c13d4636afa1258d094cd
Max W Lobato <ml93117@srcs.loc>
2017-12-13 12:05:55 -0800

    first commit

---
+++ Loopsfinal.js
@@ -0,0 +1,17 @@
+function PrimeCheck(a){
+
+    var result;
+
+
+
+    for(i = 2; i <= (number)/2; i++){
+            if(number % i == 0){
+                document.write("Not prime.");
+            }
+             document.write("Prime.");
+        }
+
+
+    }
+
+
