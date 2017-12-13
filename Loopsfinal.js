46d47e86ae6887b63dd0fad31a7547a71d7fe41e
Max W Lobato <ml93117@srcs.loc>
2017-12-13 12:18:22 -0800

    change of plans

--- Loopsfinal.js
+++ Loopsfinal.js
@@ -1,17 +1,8 @@
 function PrimeCheck(a){
 
-    var result;
 
 
 
-    for(i = 2; i <= (number)/2; i++){
-            if(number % i == 0){
-                document.write("Not prime.");
-            }
-             document.write("Prime.");
-        }
 
-
-    }
 
 
