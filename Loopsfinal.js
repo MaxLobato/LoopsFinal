7e4ac29aee39b11dba8942d367ebaf66fc3365ce
Max W Lobato <ml93117@srcs.loc>
2017-12-15 10:54:20 -0800

    timestable works now

--- Loopsfinal.js
+++ Loopsfinal.js
@@ -26,6 +26,7 @@ function TimesTable(a){
 var rows = a;
 var cells = a;
 
+document.write("<h1>");
 document.write("<table>");
     for(i = 1; i <= rows; i++){
         document.write("<tr>");
@@ -36,5 +37,6 @@ document.write("<table>");
         document.write("</tr>");
     }
 document.write("</table>");
+    document.write("</h1>");
 
 }
