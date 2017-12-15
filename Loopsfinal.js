0caffc81da9b5438d8c22fffd540a9514e0a9143
Max W Lobato <ml93117@srcs.loc>
2017-12-15 10:35:31 -0800

    timestable wokr

--- Loopsfinal.js
+++ Loopsfinal.js
@@ -1,22 +1,40 @@
 
 function PrimeCheck(a){
 
+    var result;
 
+  for(i = 2; i < (a/2); i++){
+
+    if(a%i == 0){
+      result = "not prime";
+    } else {
+        result = "Prime.";
+    }
+
+
+  }
+
+
+    document.write(result);
 
 }
-
 
 
 
 
 function TimesTable(a){
+var rows = a;
+var cells = a;
 
+document.write("<table>");
+    for(i = 1; i <= rows; i++){
+        document.write("<tr>");
 
-
-for(i = 0; i < a; i++){
-
+        for(j = 1; j <= cells; j++){
+            document.write("<td>", i * j, "</td>");
+        }
+        document.write("</tr>");
+    }
+document.write("</table>");
 
 }
-
-
-
