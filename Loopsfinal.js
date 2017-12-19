b07f233bbae4540791ab3ca771e0d8816984e188
Max W Lobato <ml93117@srcs.loc>
2017-12-19 11:19:17 -0800

    trying tomake it work

--- Loopsfinal.js
+++ Loopsfinal.js
@@ -1,7 +1,7 @@
-
 function factorTree(a) {
 
     var number = a;
+    var order = 0;
 
     document.write("<h1>");
     document.write("<table>");
@@ -9,9 +9,13 @@ function factorTree(a) {
 
     for (i = Math.ceil(number / 2); i > 1; i--) {
         document.write("<tr>");
-
+        var factor = [i];
+        order++;
         if (number % i == 0) {
-            document.write("<td>", number / i, "</td>", "<td>", i, "</td>");
+            document.write("<td>", number / factor[order], "</td>", "<td>", factor[order], "</td>");
+            if ((number / factor[order - 1]) >= 3) {
+                break;
+            }
         }
         document.write("</tr>");
     }
@@ -22,34 +26,21 @@ function factorTree(a) {
 
 
 
-function TimesTable(a){
-var rows = a;
-var cells = a;
+function TimesTable(a) {
+    var rows = a;
+    var cells = a;
 
-document.write("<h1>");
-document.write("<table>");
-    for(i = 1; i <= rows; i++){
+    document.write("<h1>");
+    document.write("<table>");
+    for (i = 1; i <= rows; i++) {
         document.write("<tr>");
 
-        for(j = 1; j <= cells; j++){
+        for (j = 1; j <= cells; j++) {
             document.write("<td>", i * j, "</td>");
         }
         document.write("</tr>");
     }
-document.write("</table>");
+    document.write("</table>");
     document.write("</h1>");
-
-}
-
-
-function factorTree(a){
-
-    for(i = Math.floor(a/2); i > 1; i--){
-
-
-
-    }
-
-
 
 }
